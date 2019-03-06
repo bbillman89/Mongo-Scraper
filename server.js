390727f20771c539aa1175c6e697ea71c3f3009b
Mtnman <billman.designs@gmail.com>
2019-03-05 20:22:10 -0500

    updates during tuesday break

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-let PORT = process.env.PORT || 3000;
-const app = express();
+const mongoose = require('mongoose');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
+let PORT = process.env.PORT || 3000;
+
+const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -14,6 +18,10 @@ app.set('view engine', 'handlebars');
 const api_routes = require('./routes/apiroutes.js');
 app.use(html_routes);
 app.use(api_routes);*/
+
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+let config = {useNewUrlParser: true};
+mongoose.connect(MONGODB_URI, config);
 
 app.listen(PORT, function(){
     console.log('Listening @ localhost:' + PORT);
